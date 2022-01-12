7656b8e7537802c2aa4182136fed0a6a172c84f8
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-01-12 12:55:39 -0800

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,6 +1,5 @@
-/*
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-*/
+
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 
 class Solution:
