74325a647931f1c6a202eef8e5ad88ae26929759
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-01-12 19:45:59 -0800

    Update 3. Longest Substring Without Repeating Characters滑动窗口.py

--- 3. Longest Substring Without Repeating Characters滑动窗口.py
+++ 3. Longest Substring Without Repeating Characters滑动窗口.py
@@ -1,1 +1,29 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # Step 1: 定义需要维护的变量, 本题求最大长度，所以需要定义max_len, 该题又涉及去重，因此还需要一个哈希表
+        max_len, hashmap = 0, {}
+
+        # Step 2: 定义窗口的首尾端 (start, end)， 然后滑动窗口
+        start = 0
+        for end in range(len(s)):
+            # Step 3
+            # 更新需要维护的变量 (max_len, hashmap)
+            # i.e. 把窗口末端元素加入哈希表，使其频率加1，并且更新最大长度
+            hashmap[s[end]] = hashmap.get(s[end], 0) + 1
+            if len(hashmap) == end - start + 1:
+                max_len = max(max_len, end - start + 1)
+            
+            # Step 4: 
+            # 根据题意,  题目的窗口长度可变: 这个时候一般涉及到窗口是否合法的问题
+            # 这时要用一个while去不断移动窗口左指针, 从而剔除非法元素直到窗口再次合法
+            # 当窗口长度大于哈希表长度时候 (说明存在重复元素)，窗口不合法
+            # 所以需要不断移动窗口左指针直到窗口再次合法, 同时提前更新需要维护的变量 (hashmap)
+            while end - start + 1 > len(hashmap):
+                head = s[start]
+                hashmap[head] -= 1
+                if hashmap[head] == 0:
+                    del hashmap[head]
+                start += 1
+        # Step 5: 返回答案 (最大长度)
+        return max_len
 
