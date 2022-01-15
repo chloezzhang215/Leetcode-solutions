82269651cec75d7545771556671261b710417f8a
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-01-15 15:52:16 -0800

    Update 496. Next Greater Element I.py

--- 496. Next Greater Element I.py
+++ 496. Next Greater Element I.py
@@ -1,1 +1,15 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = []
 
+        for i in range(len(nums1)):
+            cur = -1
+            index = nums2.index(nums1[i])
+            for j in range(index,len(nums2)):
+                if nums2[j]>nums1[i]:
+                    cur = nums2[j]
+                    break
+            res.append(cur)
+
+
+        return res
