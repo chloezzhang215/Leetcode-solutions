8dc435311c5f3102431af30874e829cca879bd9d
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-01-14 21:47:59 -0800

    Update 283. Move Zeroes.py

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -1,1 +1,14 @@
+#Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        index = 0
+        for num in nums:
+            if num != 0:
+                nums[index] = num
+                index += 1
+        for i in range(index,len(nums)):
+            nums[i] = 0
