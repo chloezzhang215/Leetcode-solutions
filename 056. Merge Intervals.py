467e4c415a14a54dd9052cb1b69b53986b3bcb02
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-01-16 09:52:35 -0800

    Update 056. Merge Intervals.py

--- 056. Merge Intervals.py
+++ 056. Merge Intervals.py
@@ -20,3 +20,28 @@ class Solution:
                 intervals[i+1][0] = min(intervals[i][0], intervals[i+1][0])    
                 
         return res 
+
+    
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> ans = new ArrayList<>();
+        Arrays.sort(intervals, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]);
+        int start = intervals[0][0], end = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++){
+            if (intervals[i][0] <= end){//两个区间有重叠
+                end = Math.max(end, intervals[i][1]);
+            }else{
+            	//两个区间没有重叠，保存[start,end]，然后更新
+                ans.add(new int[] {start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+        ans.add(new int[] {start, end});
+        int[][] res = new int[ans.size()][2];
+        for (int i = 0; i < res.length; i++){
+            res[i] = ans.get(i);
+        }
+        return res;
+    }
+}
