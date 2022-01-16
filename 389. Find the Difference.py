c151c23d2faa320dc5f8872636baf495f15eab17
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-01-15 17:43:45 -0800

    Update 389. Find the Difference.py

--- 389. Find the Difference.py
+++ 389. Find the Difference.py
@@ -1,26 +1,9 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        # 利用字典
-        # d = {}
-        # for i in s:
-        #     if i in d:
-        #         d[i] += 1
-        #     else:
-        #         d[i] = 1
-        
-        # for i in t:
-        #     if i in d:
-        #         if d[i] == 0:return i
-        #         d[i] -= 1
-        #    else:
-        #         return i
 
-        # 位运算
-        ans = 0
-        for i in s:
-            ans ^= ord(i)
-
-        for i in t:
-             ans ^= ord(i)
-        
-        return chr(ans)
+        s = sorted(list(s))
+        t = sorted(list(t))
+        for i in range(len(s)):
+            if s[i] != t[i]:
+                return t[i]
+        return t[-1]
