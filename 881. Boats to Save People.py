986a7070e1db0b23b0c4e31fd5ed4db9c37c1007
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-01-16 17:31:38 -0800

    Create 881. Boats to Save People.py

---
+++ 881. Boats to Save People.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people = sorted(people)
+        a = 0
+        b = len(people) - 1
+        count = 0
+        while a < b:
+            if people[a] + people[b] <= limit:
+                count += 1
+                a += 1
+                b -= 1
+            else:
+                b -= 1            
+        count += len(people) - count*2
+                    
+        return count
