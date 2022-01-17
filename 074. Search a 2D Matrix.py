982dddd2ce93c52c3ec48af00e18167ee48fb36b
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-01-16 21:32:42 -0800

    Update 074. Search a 2D Matrix.py

--- 074. Search a 2D Matrix.py
+++ 074. Search a 2D Matrix.py
@@ -14,3 +14,25 @@ class Solution:
                 elif matrix[i][mid] < target:
                     left = mid + 1
         return False
+
+    
+    ##不用for循环 一次二分查找
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or len(matrix)==0:
+            return None
+
+        row = len(matrix)
+        column = len(matrix[0])
+        left = 0
+        right = row * column - 1
+        while left <= right:
+            mid = left + (right-left)//2
+            x = matrix[mid//column][mid%column]
+            if x == target:
+                return True
+            elif x > target:
+                right = mid - 1
+            elif x < target:
+                left = mid + 1
+        return False
