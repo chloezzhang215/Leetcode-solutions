8b9cbc685fec6047b39c1dc1cb5efafd906d3705
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-01-17 18:11:30 -0800

    Update 053. Maximum Subarray分治or动态规划.py

--- 053. Maximum Subarray分治or动态规划.py
+++ 053. Maximum Subarray分治or动态规划.py
@@ -38,9 +38,15 @@ class Solution:
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         size = len(nums)
-        pre = 0
-        res = nums[0]
-        for i in range(size):
-            pre = max(nums[i], pre + nums[i])
-            res = max(res, pre)
-        return res
+        if size == 0:
+            return 0
+        dp = [0 for _ in range(size)]
+
+        dp[0] = nums[0]
+        for i in range(1, size):
+            if dp[i - 1] >= 0:
+                dp[i] = dp[i - 1] + nums[i]
+            else:
+                dp[i] = nums[i]
+        return max(dp)
+
