cd6ca7255ffafc0ec7d74a8fae0f26da5ea6708e
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-01-19 15:36:03 -0800

    Update 090. Subsets II回溯.py

--- 090. Subsets II回溯.py
+++ 090. Subsets II回溯.py
@@ -1,1 +1,16 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return []
+        
+        res = []
+        nums.sort()
 
+        def backtracking(index, tmp):
+            if tmp not in res:
+                res.append(tmp)
+            for i in range(index,len(nums)):
+                backtracking(i+1, tmp+[nums[i]])
+        
+        backtracking(0,[])
+        return res
