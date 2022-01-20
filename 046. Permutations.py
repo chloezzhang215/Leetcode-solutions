2d1c443381a3815a56b69ce346b7ac3a7b65bdb6
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2022-01-19 16:35:32 -0800

    Update 046. Permutations.py

--- 046. Permutations.py
+++ 046. Permutations.py
@@ -1,1 +1,27 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if not nums or len(nums) == 0:
+            return []
+
+        def dfs(nums, size, depth, path, used, res):
+            if depth == size:
+                res.append(path[:])
+                return
+
+            for i in range(size):
+                if not used[i]:
+                    used[i] = True
+                    path.append(nums[i])
+
+                    dfs(nums, size, depth + 1, path, used, res)
+
+                    used[i] = False
+                    path.pop()
+
+
+        used = [False for _ in range(len(nums))]
+        res = []
+        dfs(nums, len(nums), 0, [], used, res)
+        return res
+
 
